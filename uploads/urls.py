3fb4726a133f1a5ef86b189b260ea0c7ed92c37b
Andy Choi <joseph@igaworks.com>
2017-04-30 02:31:57 +0900

    ptern

--- uploads/urls.py
+++ uploads/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import patterns
 
 from uploads.core import views
 
